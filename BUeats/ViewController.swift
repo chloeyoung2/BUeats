b074f1e6323382ee4ffa7e8e15a02ca8d70bf987
chloe <s5105165@bournemouth.ac.uk>
2018-11-20 16:29:35 +0000

    removed comments

--- BUeats/ViewController.swift
+++ BUeats/ViewController.swift
@@ -1,18 +1,9 @@
-//
-//  ViewController.swift
-//  BUeats
-//
-//  Created by Chloe Young on 20/11/2018.
-//  Copyright © 2018 Chloe Young. All rights reserved.
-//
-
 import UIKit
-
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
 
