eb6df91713ce6d306ab0ee1c8e0cbb78673c9c55
chloe <s5105165@bournemouth.ac.uk>
2018-11-29 16:40:09 +0000

    added

--- BUeats/LoginViewController.swift
+++ BUeats/LoginViewController.swift
@@ -1,6 +1,10 @@
 import UIKit
 import Firebase
 
+@IBOutlet weak var emailTextField: UITextField!
+@IBOutlet weak var passwordTextField: UITextField!
+
+
 
 @IBAction func LoginButton(_ sender: Any) {
     guard let email = emailTextField.text, let password = passwordTextField.text else { return }
