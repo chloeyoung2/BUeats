3408b98d34bda072d775ed2fc46f85bab69fbddd
chloe <s5105165@bournemouth.ac.uk>
2018-12-06 15:29:00 +0000

    added cafe

--- BUeats/RestaurantsManager.swift
+++ BUeats/RestaurantsManager.swift
@@ -13,12 +13,27 @@ class RestaurauntsManager {
             let restaurauntsTreasure = Restauraunt(coordinate: restaurauntsCoordinate, title: "Dylans" )
             restauraunts.append(restaurauntsTreasure)
             
-        
+            let fusionCoordinate = CLLocationCoordinate2D(latitude: 50.7417838, longitude: -1.8971977)
+            let fusionTreasure = Restauraunt(coordinate: fusionCoordinate, title: "Fusion Cafe" )
+            restauraunts.append(fusionTreasure)
+       
+            let costaCoordinate = CLLocationCoordinate2D(latitude: 50.742827, longitude: -1.894646)
+            let costaTreasure = Restauraunt(coordinate: costaCoordinate, title: "Costa Coffee" )
+            restauraunts.append(costaTreasure)
     
-
+            let studentshopCoordinate = CLLocationCoordinate2D(latitude: 50.742827, longitude: -1.894646)
+            let studentshopTreasure = Restauraunt(coordinate: studentshopCoordinate, title: "Student Shop" )
+            restauraunts.append(studentshopTreasure)
+        
+            let starbucksCoordinate = CLLocationCoordinate2D(latitude: 50.742473, longitude: -1.896458)
+            let starbucksTreasure = Restauraunt(coordinate: starbucksCoordinate, title: "Starbucks" )
+            restauraunts.append(starbucksTreasure)
+        
+            let nakedcoffeeCoordinate = CLLocationCoordinate2D(latitude: 50.7424538, longitude: -1.8966498)
+            let nakedcoffeeTreasure = Restauraunt(coordinate: nakedcoffeeCoordinate, title: "Naked Coffee" )
+            restauraunts.append(nakedcoffeeTreasure)
 
     }
-    
     
     
 }
--- BUeats/TableViewController.swift
+++ BUeats/TableViewController.swift
@@ -86,3 +86,4 @@ extension TableViewController: UITableViewDataSource {
 //    }
 //
 //}
+
