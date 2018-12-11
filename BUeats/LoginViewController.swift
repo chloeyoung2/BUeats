61758ac145745a4574540744a764f850cc35e7d8
chloe <s5105165@bournemouth.ac.uk>
2018-12-11 16:02:03 +0000

    fix login

--- BUeats/LoginViewController.swift
+++ BUeats/LoginViewController.swift
@@ -1,22 +1,27 @@
 import UIKit
 import Firebase
-//
-//@IBOutlet weak var emailTextField: UITextField!
-//@IBOutlet weak var passwordTextField: UITextField!
-//
-//
-//
-//@IBAction func LoginButton(_ sender: Any) {
-//    guard let email = emailTextField.text, let password = passwordTextField.text else { return }
-//
-//    Auth.auth().signIn(withEmail: email, password: password) { user, error in
-//        if let _  = user {
-//            self.dismiss(animated: true, completion: nil)
-//        }
-//}
-//
-//}
 
-class LoginViewController:UIViewController {
+class LoginViewController: UIViewController {
     
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        emailTextField.text = "test@test.com"
+        passwordTextField.text = "chloe123"
+    }
+    
+    
+    @IBAction func LoginButton(_ sender: Any) {
+        guard let email = emailTextField.text, let password = passwordTextField.text else { return }
+
+        Auth.auth().signIn(withEmail: email, password: password) { user, error in
+            if let _  = user {
+                self.dismiss(animated: true, completion: nil)
+            }
+        }
+
+    }
 }
